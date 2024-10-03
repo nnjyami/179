cf3ffab56db0f174b192d2ca2c15c6c565047b26
nnjyami <y@179.jp>
2024-10-03 11:44:24 +0900

    initial commit

---
+++ src/components/Logo.svelte
@@ -0,0 +1,29 @@
+<p class="logo">
+	<span class="logo_1">1</span>
+	<span class="logo_2">7</span>
+	<span class="logo_3">9</span>
+</p>
+
+<style>
+	.logo {
+		color: var(--text-1);
+		font-size: clamp(2rem, calc(1.5rem + 1vw), 3rem);
+		height: 200px;
+		position: absolute;
+		top: 2em;
+		right: -0.3em;
+	}
+	.logo span {
+		display: inline-block;
+	}
+	.logo_1 {
+	}
+	.logo_2 {
+		font-size: 90%;
+		translate: -0.2em -0.26em;
+	}
+	.logo_3 {
+		font-size: 89%;
+		translate: -0.6em 0.1em;
+	}
+</style>
