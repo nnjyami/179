3733144fc19f7cefd76f7d2470fba8f7f5eb4774
nnjyami <y@179.jp>
2024-10-07 08:15:59 +0900

    初期ファイル削除、コンポーネント追加

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,36 +1,23 @@
 <script>
 	import { page } from '$app/stores';
-	import Logo from '../components/Logo.svelte';
+	import Logo from './Logo.svelte';
 </script>
 
 <header>
 	<Logo />
 
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
-				<a href="/">Home</a>
-			</li>
-			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
-				<a href="/about">About</a>
-			</li>
-			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
+	<nav></nav>
 </header>
 
 <style>
 	header {
-		display: flex;
-		justify-content: space-between;
+		overflow: hidden;
+		position: fixed;
+		top: 0;
+		left: 0;
+		z-index: 1000;
+		height: 200px;
+		width: 100%;
 	}
 
 	.corner {
--- src/components/Logo.svelte
+++ src/components/Logo.svelte
@@ -1,4 +1,4 @@
-<p class="logo">
+<p class="logo wf">
 	<span class="logo_1">1</span>
 	<span class="logo_2">7</span>
 	<span class="logo_3">9</span>
@@ -7,11 +7,14 @@
 <style>
 	.logo {
 		color: var(--text-1);
-		font-size: clamp(2rem, calc(1.5rem + 1vw), 3rem);
+		font-size: clamp(3rem, calc(2.5rem + 1vw), 4rem);
 		height: 200px;
 		position: absolute;
 		top: 2em;
-		right: -0.3em;
+		right: -0.65em;
+		font-variation-settings:
+			'wdth' 179,
+			'wght' 179;
 	}
 	.logo span {
 		display: inline-block;
@@ -20,10 +23,10 @@
 	}
 	.logo_2 {
 		font-size: 90%;
-		translate: -0.2em -0.26em;
+		translate: -0.3em -0.15em;
 	}
 	.logo_3 {
 		font-size: 89%;
-		translate: -0.6em 0.1em;
+		translate: -0.6em 0.07em;
 	}
 </style>
