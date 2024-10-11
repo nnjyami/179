5c0f3c27382c9ee758791e597b005045a6efe6de
nnjyami <y@179.jp>
2024-10-11 12:00:26 +0900

    縦書きテスト

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,7 +22,7 @@
 		flex: 1;
 		display: flex;
 		flex-direction: column;
-		padding: 1rem;
+		padding: 0;
 		width: 100%;
 		margin: 0 auto;
 		box-sizing: border-box;
---
+++ src/routes/new/+page.svelte
@@ -0,0 +1,55 @@
+<article>
+	<section>
+		<div class="inner ja-serif">
+			<p>
+				N家と宝ヶ池公園を散歩。<br />
+				途中大きなカマキリを捕まえたら、子どもたちはすっかり夢中になり、動かなくなってしまった。<br
+				/>
+				通りすがりの別の兄妹も加わり、ワイワイ眺めていた。このファミリーは英語ではない言葉を話していたけど「カマキリ」だけは日本語で、カマキリはいないのかなと気になった。<br
+				/>
+				気になったもののものの、「あなたの国にはカマキリいないの？」とは聞けなかった。
+			</p>
+		</div>
+	</section>
+	<div class="article_img">
+		<img src="/photo/L1001447.jpg" alt="" />
+	</div>
+</article>
+
+<style>
+	article {
+		width: 100vw;
+		height: 100vh;
+		position: relative;
+	}
+	.article_img {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		left: 0;
+		z-index: -1;
+	}
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+	section {
+		width: 100%;
+		height: 100svh;
+		padding: 50svh 0 10svh;
+		overflow-x: auto;
+	}
+	section .inner {
+		font-size: 34px;
+		font-feature-settings: 'vpal';
+		letter-spacing: 0.15em;
+		line-height: 2.1;
+		color: #eaebf1;
+		mix-blend-mode: difference;
+		writing-mode: vertical-rl;
+
+		padding: 0 2em;
+	}
+</style>
